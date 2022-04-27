724652a89e67ec36e1bfe7c708033b46e60ab16c
electrone901 <carbajalluis@rocketmail.com>
2022-04-26 21:13:17 -0400

    add initial react, hardhat project with basic smart contract, and connected ethers to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
+// import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+const { ethers } = require('ethers')
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [user, setUser] = useState('')
+  const [contract, setContract] = useState('')
+
+  const [balance, setBalance] = useState('')
+  const [currentAccount, setCurrentAccount] = useState('')
+  const [chainId, setChainId] = useState(0)
+  const [chainname, setChainName] = useState('')
+
+  useEffect(() => {
+    if (!currentAccount || !ethers.utils.isAddress(currentAccount)) return
+
+    //client side code
+    if (!window.ethereum) return
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    provider.getBalance(currentAccount).then((result) => {
+      setBalance(ethers.utils.formatEther(result))
+    })
+    provider.getNetwork().then((result) => {
+      setChainId(result.chainId)
+      setChainName(result.name)
+    })
+  }, [currentAccount])
+
+  const onClickConnect = () => {
+    if (!window.ethereum) {
+      console.log('please install MetaMask')
+      return
+    }
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+    // MetaMask requires requesting permission to connect users accounts
+    provider
+      .send('eth_requestAccounts', [])
+      .then((accounts) => {
+        if (accounts.length > 0) setCurrentAccount(accounts[0])
+      })
+      .catch((e) => console.log(e))
+  }
+
+  const onClickDisconnect = () => {
+    console.log('onClickDisConnect')
+    setBalance(undefined)
+    setCurrentAccount(undefined)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>My DAPP</h3>
+
+      {currentAccount ? (
+        <button onClick={onClickDisconnect}>Account:{currentAccount}</button>
+      ) : (
+        <button onClick={onClickConnect}>Connect MetaMask</button>
+      )}
+
+      {currentAccount ? (
+        <div>
+          <h3>Account info</h3>
+          <p>ETH Balance of current account: {balance}</p>
+          <p>
+            Chain Info: ChainId {chainId} name {chainname}
+          </p>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
